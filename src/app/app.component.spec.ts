7104782de1a344b6086801a107df3a17ca66db3d
Neutrino-Sunset <18445931+Neutrino-Sunset@users.noreply.github.com>
2024-12-19 21:30:13 +0000

    Converted from modules to standalone.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,13 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    imports: [AppComponent],
+}).compileComponents();
   });
 
   it('should create the app', () => {
